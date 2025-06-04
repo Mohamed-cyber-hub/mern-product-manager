d73a8e2629c8b1a6d314969c3a482e7b9a43c075
Mohamed-cyber-hub <Mohamed.saad.tradelinestores@gmail.com>
2025-06-04 14:16:55 +0300

    backed update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Adjust the target to your backend server
+        target: 'https://mern-product-manager-api.vercel.app/api', // Adjust the target to your backend server
       },
     },
   },
