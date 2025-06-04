0307f4d0644aee67a67d0f3246df39d43d5b2ae2
Mohamed-cyber-hub <Mohamed.saad.tradelinestores@gmail.com>
2025-06-04 15:15:32 +0300

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target:
+          'https://mern-product-manager-api.vercel.app/api', // Adjust the target to your backend server
+      },
+    },
+  },
 });
